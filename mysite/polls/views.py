231afe54b3d46632cc5549a0aa6318e69e68efc9
gominnam <kosok03@naver.com>
2022-05-10 10:28:12 +0900

    fix polls/views source

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,6 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Question, Choice
-from .tests import create_question
 
 
 class IndexView(generic.ListView):
@@ -67,25 +66,3 @@ class IndexView(generic.ListView):
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
-
-
-class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
-        """
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
-        """
-        past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
